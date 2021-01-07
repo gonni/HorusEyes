827a0feb7931503a42b27e7017632e682b8022c9
Jeff Kim <gonni21c@gmail.com>
2021-01-07 20:06:29 +0900

    daily commit

---
+++ src/main/java/com/yg/horus/crawl/HttpResourceCrawler.java
@@ -0,0 +1,74 @@
+package com.yg.horus.crawl;
+
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by a1000074 on 07/01/2021.
+ */
+public class HttpResourceCrawler {
+
+    public static void main(String ... v) throws Exception {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet httpget = new HttpGet("https://www.naver.com");
+
+            System.out.println("Executing request " + httpget.getRequestLine());
+
+            // Create a custom response handler
+            ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+                @Override
+                public String handleResponse(
+                        final HttpResponse response) throws ClientProtocolException, IOException {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status >= 200 && status < 300) {
+                        HttpEntity entity = response.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+
+            };
+            String responseBody = httpclient.execute(httpget, responseHandler);
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+        } finally {
+            httpclient.close();
+        }
+
+//        HttpPost httpPost = new HttpPost("http://targethost/login");
+//        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+//        nvps.add(new BasicNameValuePair("username", "vip"));
+//        nvps.add(new BasicNameValuePair("password", "secret"));
+//        httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+//        CloseableHttpResponse response2 = httpclient.execute(httpPost);
+//
+//        try {
+//            System.out.println(response2.getStatusLine());
+//            HttpEntity entity2 = response2.getEntity();
+//            // do something useful with the response body
+//            // and ensure it is fully consumed
+//            EntityUtils.consume(entity2);
+//        } finally {
+//            response2.close();
+//        }
+    }
+}
