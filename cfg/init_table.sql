dd4723f2ff09ae222eca2c04e27fc61070d8fa2f
Jeff Kim <gonni21c@gmail.com>
2021-04-14 19:05:28 +0900

    daily commit

--- cfg/init_table.sql
+++ cfg/init_table.sql
@@ -3,8 +3,14 @@ create table CRAWL_UNIT(
 	URL varchar(2048),
 	ANCHOR_TEXT varchar(1024),
 	STATUS varchar(8),
+	SEED_NO INT,
 	REG_DATE datetime,
 	UPD_DATE datetime,
 	PAGE_TEXT text(200000)
 );
 
+create table CRAWL_SEEDS(
+	SEED_NO INT not null AUTO_INCREMENT PRIMARY KEY,
+	URL_PATTERN varchar(2048),
+	TITLE varchar(128)
+);
