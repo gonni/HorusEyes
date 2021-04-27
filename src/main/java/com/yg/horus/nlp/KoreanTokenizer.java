f5cd174d0e1b9e4369407556b5e97dbd367845e7
Jeff Kim <gonni21c@gmail.com>
2021-04-27 19:11:24 +0900

    daily commit

--- src/main/java/com/yg/horus/nlp/KoreanTokenizer.java
+++ src/main/java/com/yg/horus/nlp/KoreanTokenizer.java
@@ -17,8 +17,10 @@ public class KoreanTokenizer {
         System.out.println("Active System ..") ;
 
         Komoran komoran = new Komoran(DEFAULT_MODEL.LIGHT);
+        komoran.setUserDic("cfg/kor_dic.user");
+//        komoran.setUserDic("/Users/a1000074/dev/dic/kor_dic.user");
         String strToAnalyze = "대한민국은 민주공화국이다.";
-        strToAnalyze = "방사선사였던 그는 미래가 보장된 한국에서의 삶을 뒤로 하고 2000년 초 미얀마에 정착했다";
+        strToAnalyze = "최순실은 모르토바가 호갱국의 방사선사였던 그는 미래가 보장된 한국에서의 삶을 뒤로 하고 오무아무아는 2000년 초 미얀마에 정착했다";
 
         KomoranResult analyzeResultList = komoran.analyze(strToAnalyze);
 
@@ -26,15 +28,16 @@ public class KoreanTokenizer {
 
         List<Token> tokenList = analyzeResultList.getTokenList();
         for (Token token : tokenList) {
-//            System.out.format("(%2d, %2d) %s/%s\n", token.getBeginIndex(), token.getEndIndex(), token.getMorph(), token.getPos());
+            System.out.format("(%2d, %2d) %s/%s\n", token.getBeginIndex(), token.getEndIndex(), token.getMorph(), token.getPos());
 
-            //aa
-            String pos = token.getPos();
-            String tk = token.getMorph();
+//            // Only NN
+//            String pos = token.getPos();
+//            String tk = token.getMorph();
+//
+//            if(pos != null && pos.startsWith("NN")) {
+//                System.out.println(pos + " -> " + tk) ;
+//            }
 
-            if(pos != null && pos.startsWith("NN")) {
-                System.out.println(pos + " -> " + tk) ;
-            }
 
         }
 
