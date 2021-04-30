905da72876a72f6ae7946d50cdb70ea0d3d02eaf
Jeff Kim <gonni21c@gmail.com>
2021-04-30 16:55:43 +0900

    daliy commit

--- src/main/java/com/yg/horus/api/ServiceCtr.java
+++ src/main/java/com/yg/horus/api/ServiceCtr.java
@@ -1,6 +1,7 @@
 package com.yg.horus.api;
 
 //import com.yg.horus.data.MemberRepository;
+import com.yg.horus.scheduler.JobProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class ServiceCtr {
+    @Autowired
+    private JobProducer jobProducer = null ;
 
+    @RequestMapping("/system/version")
+    public String getVersion() {
+        return "NA" ;
+    }
+
+    @RequestMapping("/crawl/unit")
+    public String execJob(long seedNo, int maxJobCnt) {
+        log.info("detected unit crawl for #{} limit {}", seedNo, maxJobCnt);
+        int scheduled = this.jobProducer.crawlContents(seedNo, maxJobCnt) ;
+
+        return String.format("Job linked with seedNo:%s scheduled : %d", seedNo, scheduled);
+    }
 //    @Autowired
 //    private MemberRepository memberRepository = null;
 //
