24c3fd95deb5df38b36212cdf3b7c98d6de71451
ygkim <gonni21c@gmail.com>
2022-09-19 22:05:15 +0900

    daily commit

--- docker_build.sh
+++ docker_build.sh
@@ -1,2 +1,2 @@
-docker build --tag horus_crawl:0.1 .
-docker run -p 8090:8090 --name horus.crawld horus_crawl:0.1
+docker build --tag horus.crawl:0.1 .
+docker run -p 8090:8090 -d --name horus.crawld --net horus_net horus.crawl:0.1
